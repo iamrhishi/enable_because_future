866be1ca93fb36305fde34e30b02a784dcdd9f6d
Vallabh Vidyasagar <vallabhvidyasagar10@gmail.com>
2026-07-25 18:53:11 +0530

    fix(backend): set default server port to 5001 in app.py

--- backend/app.py
+++ backend/app.py
@@ -670,5 +670,5 @@ def update_avatar():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 8000))
+    port = int(os.environ.get('PORT', 5001))
     app.run(debug=True, host="0.0.0.0", port=port)
